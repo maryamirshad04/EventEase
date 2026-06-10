f3e3948d21fb2d3887d186ea25b77ba52e5d2038
Maryam Irshad <156964614+maryamirshad04@users.noreply.github.com>
2026-06-10 11:46:43 +0500

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://doppleganger-1dsi.vercel.app/', 
+  baseURL: 'https://doppleganger-1dsi.vercel.app/api', 
 });
 
 api.interceptors.request.use(
