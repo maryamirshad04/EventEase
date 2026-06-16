ee09041ec38fe86d698ad8681bb454e20898a8c2
Ayka Imran <148101861+aykaimran@users.noreply.github.com>
2026-06-16 21:35:11 +0500

    Update CORS origin for frontend URL

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'https://doppleganger-xi.vercel.app',  // your frontend URL
+  origin: 'https://eventease-xi.vercel.app/',  // your frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
